777d7bebcd4b45d046fda6446e552e076d5787fb
Michael Eason <michael.eason@checkatrade.com>
2018-05-23 11:29:01 +0100

    Removed node.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-//app.use(express.static('node_modules'))
-app.use(express.static('public'))
-
-app.get('/', (req, res) => res.sendFile('./public/index.html'))
-
-app.listen(8081, () => console.log('App listening on port 8081.'))
